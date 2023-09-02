07cbfb97d552a33f25b515fa7a26e3fdd0f78ba9
Zenky <bzenky@gmail.com>
2023-09-01 21:37:22 -0300

    feat: auth navigation

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,19 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"
+import { useNavigation } from "@react-navigation/native"
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from "@components/Input"
 import { Button } from "@components/Button"
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} pb={16}>
@@ -61,6 +69,7 @@ export function SignIn() {
           <Button
             title="Criar conta"
             variant="outline"
+            onPress={handleNewAccount}
           />
         </Center>
       </VStack>
