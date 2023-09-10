a7b1658b08e7a8b4b52d8f5c16f40e8ebef73aeb
Zenky <bzenky@gmail.com>
2023-09-10 20:00:59 -0300

    feat: header exercise list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,44 @@
+import { useState } from "react"
+import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
-import { VStack } from "native-base"
+import { HStack, VStack, FlatList, Heading, Text } from "native-base"
 
 export function Home() {
+  const [groups, setGroups] = useState(['costa', 'ombro', 'bíceps', 'tríceps'])
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+          <Text color='gray.200' fontSize='sm'>
+            4
+          </Text>
+        </HStack>
+
+      </VStack>
     </VStack>
   )
 }
