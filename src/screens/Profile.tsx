96ccbb74551d623429d414af9ce9fdc8e9f9872f
Zenky <bzenky@gmail.com>
2023-09-17 13:47:18 -0300

    feat: inputs and update button - profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base"
+import { TouchableOpacity } from "react-native"
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base"
 import { ScreenHeader } from "@components/ScreenHeader"
 import { UserPhoto } from "@components/UserPhoto"
-import { TouchableOpacity } from "react-native"
+import { Input } from "@components/Input"
+import { Button } from "@components/Button"
 
 const PHOTO_SIZE = 33
 
@@ -10,10 +12,10 @@ export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
 
   return (
-    <VStack>
+    <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {
             photoIsLoading ?
@@ -36,6 +38,44 @@ export function Profile() {
               Alterar foto
             </Text>
           </TouchableOpacity>
+
+          <Input
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="E-mail"
+            isDisabled
+          />
+
+          <Heading color='gray.200' fontSize='md' mb={2} mt={12} alignSelf='flex-start'>
+            Alterar senha
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button
+            title="Atualizar"
+            mt={4}
+          />
         </Center>
       </ScrollView>
     </VStack>
