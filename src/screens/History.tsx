e324caae5ec5584aa7704d5a3605161778c809ff
Zenky <bzenky@gmail.com>
2023-09-17 11:26:55 -0300

    feat: section list empty list component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Heading, SectionList, VStack } from "native-base"
+import { Heading, SectionList, Text, VStack } from "native-base"
 import { ScreenHeader } from "@components/ScreenHeader"
 import { HistoryCard } from "@components/HistoryCard"
 
@@ -30,7 +30,15 @@ export function History() {
             {section.title}
           </Heading>
         )}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje ?
+          </Text>
+        )}
         px={8}
+        showsVerticalScrollIndicator={false}
       />
     </VStack>
   )
