fecf3916be585bdb36b15bdf930f9807e45700c8
Zenky <bzenky@gmail.com>
2023-09-17 11:18:20 -0300

    feat: static section list with history card

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,37 @@
-import { VStack } from "native-base"
+import { useState } from "react"
+import { Heading, SectionList, VStack } from "native-base"
 import { ScreenHeader } from "@components/ScreenHeader"
 import { HistoryCard } from "@components/HistoryCard"
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal']
+    }
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
 
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   )
 }
