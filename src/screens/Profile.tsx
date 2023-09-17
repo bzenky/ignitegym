b703ce0a6e10126da3dc979fec5c537ebee1aedc
Zenky <bzenky@gmail.com>
2023-09-17 13:12:00 -0300

    feat: profile avatar section

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,43 @@
-import { VStack } from "native-base"
+import { useState } from "react"
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base"
 import { ScreenHeader } from "@components/ScreenHeader"
+import { UserPhoto } from "@components/UserPhoto"
+import { TouchableOpacity } from "react-native"
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack>
       <ScreenHeader title="Perfil" />
 
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {
+            photoIsLoading ?
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded='full'
+                startColor='gray.500'
+                endColor='gray.400'
+              />
+              : <UserPhoto
+                source={{ uri: 'https://www.github.com/bzenky.png' }}
+                alt='Foto do usuário'
+                size={PHOTO_SIZE}
+              />
+          }
 
+          <TouchableOpacity>
+            <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
