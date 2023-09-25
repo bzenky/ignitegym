22b3cee7e58770344ecef1d0be0ea573d53437d7
Zenky <bzenky@gmail.com>
2023-09-25 16:42:01 -0300

    feat: input rules - error message

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -16,7 +16,7 @@ type FormDataProps = {
 export function SignUp() {
   const navigation = useNavigation()
 
-  const { control, handleSubmit } = useForm<FormDataProps>()
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>()
 
   function handleGoBack() {
     navigation.goBack()
@@ -58,6 +58,9 @@ export function SignUp() {
           <Controller
             control={control}
             name="name"
+            rules={{
+              required: 'Informe o nome.'
+            }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder="Nome"
@@ -67,9 +70,20 @@ export function SignUp() {
             )}
           />
 
+          <Text color="white">
+            {errors.name?.message}
+          </Text>
+
           <Controller
             control={control}
             name="email"
+            rules={{
+              required: 'Informe o e-mail',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder="E-mail"
@@ -80,6 +94,10 @@ export function SignUp() {
               />
             )}
           />
+
+          <Text color="white">
+            {errors.email?.message}
+          </Text>
 
           <Controller
             control={control}
