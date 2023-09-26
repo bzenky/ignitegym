ce27c8d1d84df575fcd7042ca8456938e2a6132c
Zenky <bzenky@gmail.com>
2023-09-26 10:18:41 -0300

    style: validation message and invalid input

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -66,13 +66,10 @@ export function SignUp() {
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.name?.message}
-          </Text>
 
           <Controller
             control={control}
@@ -91,13 +88,10 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.email?.message}
-          </Text>
 
           <Controller
             control={control}
