c22dc1a111dbfdbaab691135effb9c74aea0a810
bzenky <bzenky@gmail.com>
2023-12-02 18:17:56 -0300

    refactor: context user state

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
@@ -12,15 +12,15 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({
+    id: '1',
+    name: 'Zenky',
+    email: 'bzenky@gmail.com',
+    avatar: 'https://www.github.com/bzenky.png'
+  })
+
   return (
-    <AuthContext.Provider value={{
-      user: {
-        id: '1',
-        name: 'Zenky',
-        email: 'bzenky@gmail.com',
-        avatar: 'https://www.github.com/bzenky.png'
-      }
-    }}>
+    <AuthContext.Provider value={{ user }}>
       {children}
     </AuthContext.Provider>
   )
