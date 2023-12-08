0f876f2cb23fbf16bd08108ea4867529cbb688b3
bzenky <bzenky@gmail.com>
2023-12-07 21:37:52 -0300

    refactor: api baseUrl

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import { AppError } from '@utils/AppError'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.9:3333'
+  baseURL: 'http://192.168.100.12:3333'
 })
 
 // api.interceptors.request.use((config) => {
