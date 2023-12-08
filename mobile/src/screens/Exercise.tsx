7a01637da59a2f5c9ef40f61e6e4af3fb81564ee
bzenky <bzenky@gmail.com>
2023-12-07 21:48:34 -0300

    feat: register history

--- mobile/src/screens/Exercise.tsx
+++ mobile/src/screens/Exercise.tsx
@@ -22,6 +22,7 @@ export function Exercise() {
   const routes = useRoute()
   const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO)
   const [isLoading, setIsLoading] = useState(true)
+  const [isSendingRegister, setIsSendingRegister] = useState(false)
 
   const { exerciseId } = routes.params as RoutesParamsProps
   const toast = useToast()
@@ -43,6 +44,35 @@ export function Exercise() {
       })
     } finally {
       setIsLoading(false)
+    }
+  }
+
+  async function handleExerciseHistoryRegister() {
+    try {
+      setIsSendingRegister(true)
+
+      await api.post('/history', {
+        exercise_id: exerciseId
+      })
+
+      toast.show({
+        title: 'Parabéns! Exercício registrado no seu histórico.',
+        placement: 'top',
+        bgColor: 'green.700'
+      })
+
+      navigation.navigate('history')
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : 'Não foi possível registrar o exercício.'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsSendingRegister(false)
     }
   }
 
@@ -118,6 +148,8 @@ export function Exercise() {
 
                   <Button
                     title="Marcar como realizado"
+                    onPress={handleExerciseHistoryRegister}
+                    isLoading={isSendingRegister}
                   />
                 </Box>
               </VStack>
